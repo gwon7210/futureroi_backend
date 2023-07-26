370e66a3dd60049b0aa855106bdadf36838c9eba
gwon7210 <52650061+gwon7210@users.noreply.github.com>
2023-07-26 16:00:30 +0900

    oauth test 용

--- src/main/java/com/web/futureroi/controller/OauthTestController.java
+++ src/main/java/com/web/futureroi/controller/OauthTestController.java
@@ -1,10 +1,7 @@
 package com.web.futureroi.controller;
 
 import com.web.futureroi.common.CommController;
-import com.web.futureroi.service.MindTestService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
